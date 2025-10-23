c445f6d661279e358d02d8141dacfe071a641708
Fanis Chamalidis <fchamalidis@yahoo.com>
2025-10-23 21:21:43 +0300

    Added routes for user controller

--- WarehouseManagementApi/Controllers/UsersController.cs
+++ WarehouseManagementApi/Controllers/UsersController.cs
@@ -1,6 +1,107 @@
-﻿namespace WarehouseManagementApi.Controllers
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WarehouseManagementApi.Data;
+using WarehouseManagementApi.Models.User;
+
+namespace WarehouseManagementApi.Controllers
 {
-    public class UsersController
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController : ControllerBase
     {
+        private readonly AppDbContext _context;
+        public UsersController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<UserDTO>>> Get()
+        {
+            return await _context.Users
+                .Select(user => UserToDTO(user))
+                .ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<UserDTO>> GetUser(Guid id)
+        {
+            var user = await _context.Users.FindAsync(id);
+
+            if (user == null)
+                return NotFound();
+
+            return UserToDTO(user);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<UserDTO>> CreateUser(UserDTO userDTO)
+        {
+            var user = new User
+            {
+                Id = Guid.NewGuid(),
+                Username = userDTO.Username,
+                Email = userDTO.Email,
+                PasswordHash = userDTO.Password
+            };
+
+            _context.Add(user);
+            await _context.SaveChangesAsync();
+            return CreatedAtAction(nameof(CreateUser), new { id = user.Id, user });
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateUser(Guid id, UserDTO updatedUser)
+        {
+            if (id != updatedUser.Id)
+                return BadRequest();
+
+            var user = await _context.Users.FindAsync(id);
+            if (user == null)
+                return NotFound();
+
+            user.Username = updatedUser.Username;
+            user.Email = updatedUser.Email;
+            if (!string.IsNullOrEmpty(updatedUser.Password))
+            {
+                user.PasswordHash = updatedUser.Password; // Hash here if needed
+            }
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException) when (!UserExists(id))
+            {
+                return NotFound();
+            }
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteUser(Guid id)
+        {
+            var user = await _context.Users.FindAsync(id);
+
+            if (user == null)
+                return NotFound();
+
+            _context.Users.Remove(user);
+            await _context.SaveChangesAsync();
+            return NoContent();
+        }
+
+        private bool UserExists(Guid id) =>
+            _context.Users.Any(user => user.Id == id);
+
+        private static UserDTO UserToDTO(User user) =>
+            new()
+            {
+                Id = user.Id,
+                Username = user.Username,
+                Email = user.Email,
+                Password = user.PasswordHash
+            };
     }
 }
