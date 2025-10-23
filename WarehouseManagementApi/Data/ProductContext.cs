f00ce7615a1785c3f1c838d7bde6f41980a09627
Fanis Chamalidis <fchamalidis@yahoo.com>
2025-10-23 20:58:25 +0300

    refactoring of ProductContext to use primary constructor syntax, making it more concise

--- WarehouseManagementApi/Data/ProductContext.cs
+++ WarehouseManagementApi/Data/ProductContext.cs
@@ -3,12 +3,8 @@ using WarehouseManagementApi.Models;
 
 namespace WarehouseManagementApi.Data
 {
-    public class ProductContext: DbContext
+    public class ProductContext(DbContextOptions<ProductContext> options) : DbContext(options)
     {
-        public ProductContext(DbContextOptions<ProductContext> options): base(options)
-        {
-        }
-
         public DbSet<Product> Products { get; set; }
     }
 }
